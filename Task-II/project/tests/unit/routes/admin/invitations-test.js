db2b8498322d6ce8e886c2b3277c0b8e4bd354da
Nauman Shahid <nauman555@gmail.com>
2021-10-06 09:51:18 +0800

    Add files via upload

---
+++ Task-II/project/tests/unit/routes/admin/invitations-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+const { stub } = sinon;
+
+module('Unit | Route | admin/invitations', function (hooks) {
+  setupTest(hooks);
+
+  test('model hook', function (assert) {
+    const route = this.owner.factoryFor('route:admin/invitations').create({
+      store: {
+        findAll: stub().returns('invitations'),
+      },
+    });
+    assert.expect(2);
+    assert.equal(route.model(), 'invitations');
+    assert.ok(route.store.findAll.calledOnceWith('invitation'));
+  });
+});
